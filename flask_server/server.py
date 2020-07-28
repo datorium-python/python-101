a771461138e4ecb015d9228807743de3ef8ef497
Andrew Miko <andrew.miko@yandex.com>
2020-07-28 21:40:25 +0300

    Add Flask View and Template example

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,7 +1,12 @@
-from flask import Flask, request
+import random
+import datetime as dt
+from flask import Flask, request, render_template
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder='templates'
+)
 
 
 @app.route('/')
@@ -20,6 +25,31 @@ def hello():
     year = request.args.get('year')
     month = request.args.get('month')
     return f"Hello, {name} {year} {month}!"
+
+
+@app.route('/calculate/<int:a>/<int:b>')  # Route -> Registered URL in Flask
+def calculate(a, b):  # View -> Will Run Function when Route if found by Flask
+    operator = request.args.get('operator')  # Query Param is not strictly required in View function
+    if operator == 'plus':
+        result = a + b
+    elif operator == 'minus':
+        result = a - b
+    elif operator == 'multiply':
+        result = a * b
+    elif operator == 'divide':
+        result = a / b
+    else:
+        result = None
+
+    return f"Result is: {result}"
+
+
+@app.route('/template')
+def template():
+    current = dt.datetime.now()
+    users = ['Andrew', 'Kate', 'Joe', 'Dan', 'Harry']
+    random_number = random.randint(1, 10)
+    return render_template('index.html', current_datetime=current, users=users, random_number=random_number)
 
 
 app.run(
